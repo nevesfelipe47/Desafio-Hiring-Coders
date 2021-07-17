e4ed632b4aeb424503300f93c68d80b4e6a8744e
nevesfelipe47 <santosfelpe47@gmail.com>
2021-07-17 17:59:35 -0300

    Alteração de data final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import Countdown from "./countdown.js";
 
-const tempoParaBf = new Countdown("25 July 2021 20:00:00 GMT-0300");
+const tempoParaBf = new Countdown("30 July 2021 20:00:00 GMT-0300");
 console.log(tempoParaBf.total);
 
 const tempos = document.querySelectorAll("[data-time]");
